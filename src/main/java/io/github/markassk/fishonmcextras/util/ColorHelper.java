ee56b925b21f28585cc978820878948e03174b8d
MadisonMadi <thedarkmoonminrecraft@gmail.com>
2025-12-24 20:51:43 +0100

    fixes

--- src/main/java/io/github/markassk/fishonmcextras/util/ColorHelper.java
+++ src/main/java/io/github/markassk/fishonmcextras/util/ColorHelper.java
@@ -4,10 +4,13 @@ import me.shedaniel.math.Color;
 
 public class ColorHelper {
     public static int getColorFromNbt(String value) {
-        int r = Integer.parseInt(value.substring(0, value.indexOf(",")).trim());
-        int g = Integer.parseInt(value.substring(value.indexOf(",") + 1, TextHelper.ordinalIndexOf(value, ",", 2)).trim());
-        int b = Integer.parseInt(value.substring(value.lastIndexOf(",") + 1).trim());
-
-        return Color.ofRGB(r, g, b).getColor();
+        try {
+            int r = Integer.parseInt(value.substring(0, value.indexOf(",")).trim());
+            int g = Integer.parseInt(value.substring(value.indexOf(",") + 1, TextHelper.ordinalIndexOf(value, ",", 2)).trim());
+            int b = Integer.parseInt(value.substring(value.lastIndexOf(",") + 1).trim());
+            return Color.ofRGB(r, g, b).getColor();
+        } catch (Exception e) {
+            return Color.ofRGB(255, 255, 255).getColor();
+        }
     }
 }
